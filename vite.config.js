a91fad52a4f88adc776205648c67920687f89eb9
vishvchavan07 <vishvcode@gmail.com>
2026-03-12 22:48:46 +0530

    fix: set vite base path for github pages and add gh-pages package

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/SPENTWELL_1.0/',
   plugins: [react()],
   build: {
     outDir: 'dist',
